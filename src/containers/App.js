286a9d00874dbea66e47f2dc23ce9394b4c573e3
ammanuel <ammanueladdis@gmail.com>
2019-08-04 08:16:48 +1200

    starting udemy 82-middleware. redux connected successfully - sending/receiving state successfully

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,6 +13,8 @@ import "./App.css";
 
 import { setSearchField } from '../actions'
 
+//TODO: rememner sending and receiving state functions below - also different export at end of file
+
 const mapStateToProps = state => {
   return {
     searchField: state.searchField
@@ -29,33 +31,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchfield: ""
+      robots: []
     };
   }
 
   componentDidMount() {
-    console.log(this.props.store.getState())    
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users => this.setState({ robots: users }));
   }
 
-  onSearchChange = e => {
-    this.setState({ searchfield: e.target.value });
-  };
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
       return !robots.length ? 
       <h1>Loading... yikes duuuuuude!</h1> :
       (      
         <div className="tc">
           <h1 className="f1">Robo Friends - omg yay!</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots} />      
